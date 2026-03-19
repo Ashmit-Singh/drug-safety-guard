50771223638e027a098b436d82bed66bf4cb08f6
Ashmit Singh <connect.ashmit.singh@proton.me>
2026-03-19 23:04:15 +0530

    fix(flutter): inline vercel build script to prevent CRLF errors

--- flutter_app/vercel_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Installing Flutter..."
-if [ ! -d "flutter" ]; then
-  git clone https://github.com/flutter/flutter.git -b stable
-fi
-export PATH="$PATH:`pwd`/flutter/bin"
-echo "Building Flutter Web App..."
-flutter build web --release
